a8b161078b280b42bc15e5d94527235ec87fc5a6
Josh <josh@fortestrong.com.au>
2019-02-24 16:31:02 +1100

    Fixes for personal project

---
+++ src/Send.js
@@ -0,0 +1,67 @@
+/* eslint no-use-before-define: ["error", { "variables": false }] */
+
+import PropTypes from "prop-types";
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View, ViewPropTypes } from "react-native";
+import Color from "./Color";
+
+export default function Send({ text, containerStyle, onSend, children, textStyle, label, alwaysShowSend, disabled }) {
+    if (alwaysShowSend || text.trim().length > 0) {
+        return (
+            <TouchableOpacity
+                testID="send"
+                accessible
+                accessibilityLabel="send"
+                style={[styles.container, containerStyle]}
+                onPress={() => {
+                    onSend({ text: text.trim() }, true);
+                }}
+                accessibilityTraits="button"
+                disabled={disabled}
+            >
+                <View style={{ justifyContent: "center", alignItems: "center" }}>
+                    {children || <Text style={[styles.text, textStyle]}>{label}</Text>}
+                </View>
+            </TouchableOpacity>
+        );
+    }
+    return <View />;
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 44,
+        justifyContent: "center"
+    },
+    text: {
+        color: Color.defaultBlue,
+        fontWeight: "600",
+        fontSize: 17,
+        backgroundColor: Color.backgroundTransparent,
+        marginBottom: 12,
+        marginLeft: 10,
+        marginRight: 10
+    }
+});
+
+Send.defaultProps = {
+    text: "",
+    onSend: () => {},
+    label: "Send",
+    containerStyle: {},
+    textStyle: {},
+    children: null,
+    alwaysShowSend: false,
+    disabled: false
+};
+
+Send.propTypes = {
+    text: PropTypes.string,
+    onSend: PropTypes.func,
+    label: PropTypes.string,
+    containerStyle: ViewPropTypes.style,
+    textStyle: Text.propTypes.style,
+    children: PropTypes.element,
+    alwaysShowSend: PropTypes.bool,
+    disabled: PropTypes.bool
+};
